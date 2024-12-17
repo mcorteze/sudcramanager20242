f8baf5da6eb179a11df5143e8ca3cee5da150cdc
mcorteze <mcorteze23@gmail.com>
2024-12-17 17:12:42 -0300

    12171712

--- src/components/ImagenPage/ErroresTable.js
+++ src/components/ImagenPage/ErroresTable.js
@@ -4,13 +4,13 @@ import { Table } from 'antd';
 const ErroresTable = ({ erroresData, loading }) => {
   const erroresColumns = [
     { title: 'ID Error', dataIndex: 'id_error', key: 'id_error' },
-    { title: 'RUT', dataIndex: 'rut', key: 'rut' },
-    { title: 'Num Prueba', dataIndex: 'num_prueba', key: 'num_prueba' },
-    { title: 'Cod Interno', dataIndex: 'cod_interno', key: 'cod_interno' },
     { title: 'ID Archivo Leído', dataIndex: 'id_archivoleido', key: 'id_archivoleido' },
     { title: 'Imagen', dataIndex: 'imagen', key: 'imagen' },
-    { title: 'Instante Forms', dataIndex: 'instante_forms', key: 'instante_forms' },
     { title: 'Valida RUT', dataIndex: 'valida_rut', key: 'valida_rut', render: (text) => text ? 'Sí' : 'No' },
+    { title: 'Valida Matrícula', dataIndex: 'valida_matricula', key: 'valida_matricula', render: (text) => text ? 'Sí' : 'No' },
+    { title: 'Valida Inscripción', dataIndex: 'valida_inscripcion', key: 'valida_inscripcion', render: (text) => text ? 'Sí' : 'No' },
+    { title: 'Valida Eval', dataIndex: 'valida_eval', key: 'valida_eval', render: (text) => text ? 'Sí' : 'No' },
+    { title: 'Valida Forma', dataIndex: 'valida_forma', key: 'valida_forma', render: (text) => text ? 'Sí' : 'No' },
   ];
 
   return (
